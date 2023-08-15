d03a7ca1aace602031edac95d7e7f563966ea4ee
pizhlo <zaush@bk.ru>
2023-08-15 19:55:47 +0300

    added new error: unique violation

--- pkg/errors.go
+++ pkg/errors.go
@@ -6,3 +6,4 @@ type Error string
 func (e Error) Error() string { return string(e) }
 
 const TokenNotExist = Error("Token does not exist")
+const uniqueViolation = Error(`ERROR: duplicate key value violates unique constraint "urls_original_url_idx" (SQLSTATE 23505)`)
