06d7e6776f20183a7035c0f41bf1bb209e471c1a
pizhlo <zaush@bk.ru>
2023-08-18 15:23:38 +0300

    added new error: no orders

--- pkg/errors.go
+++ pkg/errors.go
@@ -7,4 +7,5 @@ func (e Error) Error() string { return string(e) }
 
 const TokenNotExist = Error("Token does not exist")
 const UniqueViolationCode = "23505"
-const uniqueViolation = Error(`ERROR: duplicate key value violates unique constraint "urls_original_url_idx" (SQLSTATE 23505)`)
+const uniqueViolation = Error(`ERROR: duplicate key value violates unique constraint "urls_original_url_idx" (SQLSTATE 23505)`) // тут нужно вставить реальную ошибку
+const NoOrders = Error("User doesn't have any orders")
